0f426c0f8f40ab098331fea2a49d1f68514f3aa0
Aurélien Pupier <apupier@redhat.com>
2022-10-18 10:56:00 +0200

    feat: Include opt-in Red Hat telemetry
    followed documentation from
    https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-use-this-library
    
    currently tracking only generic data on activation and deactivation of
    the extension
    
    fixes #25
    
    next interesting thing to track:
    - opening of the Kaoto editor

--- webpack.config.js
+++ webpack.config.js
@@ -87,6 +87,7 @@ const commonConfig = (env) => {
         },
       ],
     },
+	ignoreWarnings: [/Failed to parse source map/],
     output: {
       path: path.resolve("./dist"),
       filename: "[name].js",
@@ -105,7 +106,7 @@ const commonConfig = (env) => {
       maxEntrypointSize: 30000000,
     },
     resolve: {
-      // Required for github.dev and `minimatch`, as Webpack 5 doesn't add polyfills automatically anymore.
+      // Required for github.dev, `minimatch` and vscode-redhat-telemetry, as Webpack 5 doesn't add polyfills automatically anymore.
       fallback: {
         path: require.resolve("path-browserify"),
         os: require.resolve("os-browserify/browser"),
@@ -113,6 +114,10 @@ const commonConfig = (env) => {
         child_process: false,
         net: false,
         buffer: require.resolve("buffer/"),
+		assert: false,
+		process: false,
+		stream: false,
+		util: false
       },
       extensions: [".tsx", ".ts", ".js", ".jsx"],
       modules: ["node_modules"],
