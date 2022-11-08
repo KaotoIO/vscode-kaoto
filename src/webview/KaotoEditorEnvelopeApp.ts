029e5482eb6bf8befbd109cc1c1ce795da3bb9aa
Aurélien Pupier <apupier@redhat.com>
2022-11-08 14:29:42 +0100

    Change dependency name from kaoto-ui to @kaoto/kaoto-ui
    fixes #66
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/webview/KaotoEditorEnvelopeApp.ts
+++ src/webview/KaotoEditorEnvelopeApp.ts
@@ -1,4 +1,4 @@
-import { KaotoEditorFactory } from "kaoto-ui/dist/lib/kogito-integration";
+import { KaotoEditorFactory } from "@kaoto/kaoto-ui/dist/lib/kogito-integration";
 import * as EditorEnvelope from "@kie-tools-core/editor/dist/envelope";
 
 declare const acquireVsCodeApi: any;
