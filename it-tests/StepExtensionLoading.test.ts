e2f3977a18b55106127793b4423a51f7a0bc2238
Aurélien Pupier <apupier@redhat.com>
2023-03-09 16:18:14 +0100

    Workaround potential redraw of canvas before clicking on Choice step
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- it-tests/StepExtensionLoading.test.ts
+++ it-tests/StepExtensionLoading.test.ts
@@ -1,6 +1,7 @@
 import { By, EditorView,  until,  VSBrowser, WebDriver } from 'vscode-extension-tester';
 import * as path from 'path';
 import { openAndSwitchToKaotoFrame } from './Util';
+import waitUntil from 'async-wait-until';
 
 describe('Step extension loading test', function () {
 	this.timeout(25000);
@@ -24,7 +25,17 @@ describe('Step extension loading test', function () {
 		const { kaotoWebview, kaotoEditor } = await openAndSwitchToKaotoFrame(workspaceFolder, 'choice.camel.yaml', driver, true);
 		const stepChoiceXpath = By.xpath("//div[@data-testid='viz-step-choice']");
 		await driver.wait(until.elementLocated(stepChoiceXpath));
-		await (await driver.findElement(stepChoiceXpath)).click();
+
+		await waitUntil(async () => {
+			try {
+				await (await driver.findElement(stepChoiceXpath)).click();
+			} catch {
+				console.log('Clicked on step failed surely due to Kaoto UI redrawing the content of the canvas. Will retry as a workaround');
+				return false;
+			}
+			return true;
+		}, 10000, 2000);
+
 		await driver.wait(until.elementLocated(By.xpath("//button[@data-testid='choice-add-when-button']")));
 		await kaotoWebview.switchBack();
 	});
