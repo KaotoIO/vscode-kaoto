edb5efb329651550db5a65d013c1a8c3b7473c42
Ricardo M <lordrip@gmail.com>
2023-06-13 11:47:17 +0200

    test: increase timeouts to ensure that the test executes

--- it-tests/BasicFlow.test.ts
+++ it-tests/BasicFlow.test.ts
@@ -5,91 +5,91 @@ import { checkEmptyCanvasLoaded, openAndSwitchToKaotoFrame } from './Util';
 import { waitUntil } from 'async-wait-until';
 
 describe('Kaoto basic development flow', function () {
-	this.timeout(50000);
+  this.timeout(60_000);
 
-	const workspaceFolder = path.join(__dirname, '../test Fixture with speci@l chars');
+  const workspaceFolder = path.join(__dirname, '../test Fixture with speci@l chars');
 
-	let driver: WebDriver;
+  let driver: WebDriver;
 
-	before(async function() {
-		this.timeout(20000);
-		driver = VSBrowser.instance.driver;
-		await VSBrowser.instance.openResources(workspaceFolder);
-	});
+  before(async function() {
+    this.timeout(60_000);
+    driver = VSBrowser.instance.driver;
+    await VSBrowser.instance.openResources(workspaceFolder);
+  });
 
-	afterEach(async function() {
-		const editorView = new EditorView();
-		await editorView.closeAllEditors();
-	});
+  afterEach(async function() {
+    const editorView = new EditorView();
+    await editorView.closeAllEditors();
+  });
 
-	it('Open "emptyKameletBinding.kaoto.yaml" file and check Kaoto UI is loading', async function () {
-		// Ugly workaround to wait that VS Code instance and its extensions have started before playing with it.
-		// This is the first test to be played so putting this sleep only here.
-		// See https://github.com/KaotoIO/vscode-kaoto/issues/202 to fix issue related to that
-		await driver.sleep(5000);
+  it('Open "emptyKameletBinding.kaoto.yaml" file and check Kaoto UI is loading', async function () {
+    // Ugly workaround to wait that VS Code instance and its extensions have started before playing with it.
+    // This is the first test to be played so putting this sleep only here.
+    // See https://github.com/KaotoIO/vscode-kaoto/issues/202 to fix issue related to that
+    await driver.sleep(5_000);
 
-		const { kaotoWebview, kaotoEditor } = await openAndSwitchToKaotoFrame(workspaceFolder, 'emptyKameletBinding.kaoto.yaml', driver, true);
-		await checkIntegrationNameInTopBarLoaded(driver, 'my-integration-name');
-		await checkEmptyCanvasLoaded(driver);
-		await kaotoWebview.switchBack();
-		assert.isFalse(await kaotoEditor.isDirty(), 'The Kaoto editor should not be dirty after everything has loaded.');
-	});
+    const { kaotoWebview, kaotoEditor } = await openAndSwitchToKaotoFrame(workspaceFolder, 'emptyKameletBinding.kaoto.yaml', driver, true);
+    await checkIntegrationNameInTopBarLoaded(driver, 'my-integration-name');
+    await checkEmptyCanvasLoaded(driver);
+    await kaotoWebview.switchBack();
+    assert.isFalse(await kaotoEditor.isDirty(), 'The Kaoto editor should not be dirty after everything has loaded.');
+  });
 
-	it('Open "empty.camel.yaml" file, check Kaoto UI is loading, add a step and save', async function () {
-		let { kaotoWebview, kaotoEditor } = await openAndSwitchToKaotoFrame(workspaceFolder, 'empty.camel.yaml', driver, false);
-		await checkEmptyCanvasLoaded(driver);
-		await addActiveMQStep(driver);
-		await checkStepWithTestIdPresent(driver, 'viz-step-activemq');
+  it('Open "empty.camel.yaml" file, check Kaoto UI is loading, add a step and save', async function () {
+    let { kaotoWebview, kaotoEditor } = await openAndSwitchToKaotoFrame(workspaceFolder, 'empty.camel.yaml', driver, false);
+    await checkEmptyCanvasLoaded(driver);
+    await addActiveMQStep(driver);
+    await checkStepWithTestIdPresent(driver, 'viz-step-activemq');
 
-		await kaotoWebview.switchBack();
-		assert.isTrue(await kaotoEditor.isDirty(), 'The Kaoto editor should be dirty after adding a step.');
-		console.log('will save');
-		await kaotoEditor.save();
-		await waitUntil(async() => {
-			return !(await kaotoEditor.isDirty());
-		});
-		console.log('editor no more dirty');
+    await kaotoWebview.switchBack();
+    assert.isTrue(await kaotoEditor.isDirty(), 'The Kaoto editor should be dirty after adding a step.');
+    console.log('will save');
+    await kaotoEditor.save();
+    await waitUntil(async() => {
+      return !(await kaotoEditor.isDirty());
+    });
+    console.log('editor no more dirty');
 
-		const editorView = new EditorView();
-		await editorView.closeAllEditors();
-		console.log('editors closed');
+    const editorView = new EditorView();
+    await editorView.closeAllEditors();
+    console.log('editors closed');
 
-		({ kaotoWebview, kaotoEditor } = await openAndSwitchToKaotoFrame(workspaceFolder, 'empty.camel.yaml', driver, true));
-		await checkStepWithTestIdPresent(driver, 'viz-step-activemq');
-		await kaotoWebview.switchBack();
-	});
+    ({ kaotoWebview, kaotoEditor } = await openAndSwitchToKaotoFrame(workspaceFolder, 'empty.camel.yaml', driver, true));
+    await checkStepWithTestIdPresent(driver, 'viz-step-activemq');
+    await kaotoWebview.switchBack();
+  });
 
-	it('Open Camel file and check Kaoto UI is loading', async function () {
-		const { kaotoWebview, kaotoEditor } = await openAndSwitchToKaotoFrame(workspaceFolder, 'my.camel.yaml', driver, true);
-		await checkStepWithTestIdPresent(driver, 'viz-step-timer');
-		await checkStepWithTestIdPresent(driver, 'viz-step-log');
-		await kaotoWebview.switchBack();
-		assert.isFalse(await kaotoEditor.isDirty(), 'The Kaoto editor should not be dirty after everything has loaded.');
-	});
+  it('Open Camel file and check Kaoto UI is loading', async function () {
+    const { kaotoWebview, kaotoEditor } = await openAndSwitchToKaotoFrame(workspaceFolder, 'my.camel.yaml', driver, true);
+    await checkStepWithTestIdPresent(driver, 'viz-step-timer');
+    await checkStepWithTestIdPresent(driver, 'viz-step-log');
+    await kaotoWebview.switchBack();
+    assert.isFalse(await kaotoEditor.isDirty(), 'The Kaoto editor should not be dirty after everything has loaded.');
+  });
 });
 
 async function addActiveMQStep(driver: WebDriver) {
-	await waitUntil(async () => {
-		try {
-			await clickOnAddAStep(driver);
-		} catch {
-			console.log('Clicked on step failed surely due to Kaoto UI redrawing the content of the canvas. Will retry as a workaround');
-			return false;
-		}
-		return true;
-	}, 10000, 2000);
-	await driver.wait(until.elementLocated(By.xpath("//button[@data-testid='miniCatalog__stepItem--activemq']")));
-	await (await driver.findElement(By.xpath("//button[@data-testid='miniCatalog__stepItem--activemq']"))).click();
+  await waitUntil(async () => {
+    try {
+      await clickOnAddAStep(driver);
+    } catch {
+      console.log('Clicked on step failed surely due to Kaoto UI redrawing the content of the canvas. Will retry as a workaround');
+      return false;
+    }
+    return true;
+  }, 10_000, 2_000);
+  await driver.wait(until.elementLocated(By.xpath("//button[@data-testid='miniCatalog__stepItem--activemq']")));
+  await (await driver.findElement(By.xpath("//button[@data-testid='miniCatalog__stepItem--activemq']"))).click();
 }
 
 async function clickOnAddAStep(driver: WebDriver) {
-	await (await driver.findElement(By.xpath("//div[@data-testid='viz-step-slot']"))).click();
+  await (await driver.findElement(By.xpath("//div[@data-testid='viz-step-slot']"))).click();
 }
 
 async function checkStepWithTestIdPresent(driver: WebDriver, testId: string) {
-	await driver.wait(until.elementLocated(By.xpath(`//div[@data-testid='${testId}']`)));
+  await driver.wait(until.elementLocated(By.xpath(`//div[@data-testid='${testId}']`)));
 }
 
 async function checkIntegrationNameInTopBarLoaded(driver: WebDriver, name: string) {
-	await driver.wait(until.elementLocated(By.xpath(`//span[text()='${name}']`)));
+  await driver.wait(until.elementLocated(By.xpath(`//span[text()='${name}']`)));
 }
--- it-tests/StepExtensionLoading.test.ts
+++ it-tests/StepExtensionLoading.test.ts
@@ -4,40 +4,40 @@ import { openAndSwitchToKaotoFrame } from './Util';
 import waitUntil from 'async-wait-until';
 
 describe('Step extension loading test', function () {
-	this.timeout(25000);
+  this.timeout(60_000);
 
-	const workspaceFolder = path.join(__dirname, '../test Fixture with speci@l chars');
+  const workspaceFolder = path.join(__dirname, '../test Fixture with speci@l chars');
 
-	let driver: WebDriver;
+  let driver: WebDriver;
 
-	before(async function() {
-		this.timeout(20000);
-		driver = VSBrowser.instance.driver;
-		await VSBrowser.instance.openResources(workspaceFolder);
-	});
+  before(async function() {
+    this.timeout(60_000);
+    driver = VSBrowser.instance.driver;
+    await VSBrowser.instance.openResources(workspaceFolder);
+  });
 
-	afterEach(async function() {
-		const editorView = new EditorView();
-		await editorView.closeAllEditors();
-	});
+  afterEach(async function() {
+    const editorView = new EditorView();
+    await editorView.closeAllEditors();
+  });
 
-	it('Open "choice.camel.yaml" file and check Step extension is loading', async function () {
-		const { kaotoWebview, kaotoEditor } = await openAndSwitchToKaotoFrame(workspaceFolder, 'choice.camel.yaml', driver, true);
-		const stepChoiceXpath = By.xpath("//div[@data-testid='viz-step-choice']");
-		await driver.wait(until.elementLocated(stepChoiceXpath));
+  it('Open "choice.camel.yaml" file and check Step extension is loading', async function () {
+    const { kaotoWebview, kaotoEditor } = await openAndSwitchToKaotoFrame(workspaceFolder, 'choice.camel.yaml', driver, true);
+    const stepChoiceXpath = By.xpath("//div[@data-testid='viz-step-choice']");
+    await driver.wait(until.elementLocated(stepChoiceXpath));
 
-		await waitUntil(async () => {
-			try {
-				await (await driver.findElement(stepChoiceXpath)).click();
-			} catch {
-				console.log('Clicked on step failed surely due to Kaoto UI redrawing the content of the canvas. Will retry as a workaround');
-				return false;
-			}
-			return true;
-		}, 10000, 2000);
+    await waitUntil(async () => {
+      try {
+        await (await driver.findElement(stepChoiceXpath)).click();
+      } catch {
+        console.log('Clicked on step failed surely due to Kaoto UI redrawing the content of the canvas. Will retry as a workaround');
+        return false;
+      }
+      return true;
+    }, 10_000, 2_000);
 
-		await driver.wait(until.elementLocated(By.xpath("//button[@data-testid='choice-add-when-button']")));
-		await kaotoWebview.switchBack();
-	});
+    await driver.wait(until.elementLocated(By.xpath("//button[@data-testid='choice-add-when-button']")));
+    await kaotoWebview.switchBack();
+  });
 
 });
