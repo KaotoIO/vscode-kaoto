50de5b09626f8abf2064e2ccda763d15993b6a53
Dominik Jelinek <djelinek@redhat.com>
2023-07-20 15:25:05 +0200

    Throw error when trying to download non existing Kaoto backend binary
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,17 +2,33 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
 
+class HTTPResponseError extends Error {
+	constructor(response) {
+		super(`${response.status} ${response.statusText} - ${response.url}`);
+	}
+}
+
+const checkStatus = (response) => {
+	if (response.ok) {
+		return response;
+	} else {
+		throw new HTTPResponseError(response);
+	}
+}
+
 const downloadFile = (async (url, filePath) => {
   console.log(`Will fetch backend binary from ${url} into ${path.resolve(filePath)}`);
   const res = await fetch(url);
-  const fileStream = fs.createWriteStream(filePath);
-  await new Promise((resolve, reject) => {
-      res.body.pipe(fileStream);
-      res.body.on("error", reject);
-      fileStream.on("finish", resolve);
-  });
-  console.log(`Binary downloaded ${path.resolve(filePath)}`)
-  fs.chmodSync(filePath, "766");
+  if(checkStatus(res)) {
+    const fileStream = fs.createWriteStream(filePath);
+    await new Promise((resolve, reject) => {
+        res.body.pipe(fileStream);
+        res.body.on("error", reject);
+        fileStream.on("finish", resolve);
+    });
+    console.log(`Binary downloaded ${path.resolve(filePath)}`)
+    fs.chmodSync(filePath, "766");
+  }
 });
 
 const downloadKaotoBackendNativeExecutable = (backendVersion, platform, extension) => {
