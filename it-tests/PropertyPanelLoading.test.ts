95c8592330c029b7bd9e56962ec50aa54870ca60
Dominik Jelinek <djelinek@redhat.com>
2023-11-27 21:33:33 +0100

    fix property panel loading test
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- it-tests/PropertyPanelLoading.test.ts
+++ it-tests/PropertyPanelLoading.test.ts
@@ -1,4 +1,4 @@
-import { By, EditorView, until, VSBrowser, WebDriver } from 'vscode-extension-tester';
+import { By, EditorView, until, VSBrowser, WebDriver, WebView } from 'vscode-extension-tester';
 import * as path from 'path';
 import { openAndSwitchToKaotoFrame } from './Util';
 
@@ -8,30 +8,46 @@ describe('property panel loading test', function () {
   const workspaceFolder = path.join(__dirname, '../test Fixture with speci@l chars');
 
   let driver: WebDriver;
+  let kaotoWebview: WebView;
 
   before(async function () {
     this.timeout(60_000);
     driver = VSBrowser.instance.driver;
   });
 
-  afterEach(async function () {
+  after(async function () {
+    await kaotoWebview.switchBack();
+
     const editorView = new EditorView();
     await editorView.closeAllEditors();
   });
 
-  it('Open "choice.camel.yaml" file and check property panel is loading', async function () {
-    const { kaotoWebview, } = await openAndSwitchToKaotoFrame(
+  it('Open "choice.camel.yaml" file and check property panel is loading and closing', async function () {
+    kaotoWebview = (await openAndSwitchToKaotoFrame(
       workspaceFolder,
       'choice.camel.yaml',
       driver,
       true
-    );
-    const stepWhenXpath = By.xpath("//g[@data-testid='custom-node__when-*']");
-    await driver.wait(until.elementLocated(stepWhenXpath));
+    )).kaotoWebview;
+    const stepWhenXpath = By.xpath(`//\*[name()='g' and starts-with(@data-testid,'custom-node__when')]`)
+    await driver.wait(until.elementLocated(stepWhenXpath), 5_000);
     await (await driver.findElement(stepWhenXpath)).click();
     await driver.wait(
-      until.elementLocated(By.xpath("//input[@value='${header.foo} == 1']"))
-    );
-    await kaotoWebview.switchBack();
+      until.elementLocated(By.className('pf-v5-c-card')
+    ), 5_000);
+
+    console.log('try to close property panel');
+    await (await driver.findElement(By.xpath("//button[@data-testid='close-side-bar']"))).click();
+    console.log('wait until property panel is no located');
+    try {
+      await driver.wait(
+        until.elementLocated(By.className('pf-v5-c-card')
+      ), 5_000);
+      throw new Error('Property panel was not closed!')
+    } catch (error) {
+      if(error.name !== 'TimeoutError') {
+        throw new Error(error.message);
+      }
+    }
   });
 });
