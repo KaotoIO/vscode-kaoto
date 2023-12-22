557730fdb73b99688b5e9176d95014895bd09e4d
Dominik Jelínek <djelinek@redhat.com>
2023-12-22 08:52:03 +0100

    Add initial tests for switching between editor tabs (#431)

---
+++ it-tests/SwitchBetweenTabs.test.ts
@@ -0,0 +1,101 @@
+import { By, EditorView, until, VSBrowser, WebDriver, WebElement, WebView } from 'vscode-extension-tester';
+import { openAndSwitchToKaotoFrame } from './Util';
+import { join } from 'path';
+import { expect } from 'chai';
+
+describe('Switching between editor tabs', function () {
+	this.timeout(90_000)
+
+	const WORKSPACE_FOLDER = join(__dirname, '../test Fixture with speci@l chars');
+
+	const locators = {
+		EditorTabs: {
+			tabsList: 'pf-v5-c-tabs__list',
+			tabsItem: 'pf-v5-c-tabs__item',
+			activeTabAttribute: 'aria-selected',
+			tabLabelAttribute: 'aria-label',
+			tabText: 'pf-v5-c-tabs__item-text',
+			designTabLabel: 'Design canvas',
+			beansTabLabel: 'Beans editor',
+		},
+		DesignCanvas: {
+			canvas: 'topology'
+		},
+		BeansEditor: {
+			listView: 'metadata-editor-modal-list-view',
+			detailsView: 'metadata-editor-modal-details-view'
+		}
+	}
+
+	let driver: WebDriver;
+	let kaotoWebview: WebView;
+
+	before(function () {
+		driver = VSBrowser.instance.driver;
+	});
+
+	after(async function () {
+		await kaotoWebview.switchBack();
+		await new EditorView().closeAllEditors();
+	});
+
+	it('Open "my.camel.yaml" file and check "Design" tab is active by default', async function () {
+		kaotoWebview = (await openAndSwitchToKaotoFrame(
+			WORKSPACE_FOLDER,
+			'my.camel.yaml',
+			driver,
+			true
+		)).kaotoWebview;
+		expect(await getTabsCount()).to.equal(2);
+		expect(await getActiveTabName()).to.equal('Design');
+	});
+
+	it('Switch to "Beans" tab and check it is active', async function () {
+		await switchToTab(locators.EditorTabs.beansTabLabel);
+		await waitForBeansEditorIsLoaded();
+		expect(await getActiveTabName()).to.equal('Beans');
+	});
+
+	it('Switch back to "Design" tab and check it is active', async function () {
+		await switchToTab(locators.EditorTabs.designTabLabel);
+		await waitForDesignCanvasIsLoaded();
+		expect(await getActiveTabName()).to.equal('Design');
+	});
+
+	async function getTabsWebElements(): Promise<WebElement[]> {
+		return await driver.findElement(By.className(locators.EditorTabs.tabsList)).findElements(By.className(locators.EditorTabs.tabsItem));
+	}
+
+	async function getTabsCount(): Promise<number> {
+		await driver.wait(
+			until.elementLocated(By.className(locators.EditorTabs.tabsList)),
+			5_000, 'Editor tabs are not displayed properly!'
+		);
+		return (await getTabsWebElements()).length;
+	}
+
+	async function getActiveTabName(): Promise<string> {
+		const tabName = await driver.findElement(By.className(locators.EditorTabs.tabsList)).findElement(By.xpath(`//button[@${locators.EditorTabs.activeTabAttribute}='true']`)).findElement(By.className(locators.EditorTabs.tabText));
+		return await tabName.getText();
+	}
+
+	async function switchToTab(name: string): Promise<void> {
+		await driver.findElement(By.className(locators.EditorTabs.tabsList)).findElement(By.xpath(`//button[@${locators.EditorTabs.tabLabelAttribute}='${name}']`)).click();
+	}
+
+	async function waitForDesignCanvasIsLoaded(): Promise<void> {
+		await driver.wait(
+			until.elementLocated(By.xpath(`//div[@data-test-id='${locators.DesignCanvas.canvas}']`)),
+			5_000, 'Design canvas of editor was not loaded properly!'
+		);
+	}
+
+	async function waitForBeansEditorIsLoaded(): Promise<void> {
+		await driver.wait(until.elementLocated(By.className(locators.BeansEditor.listView)),
+			5_000, 'Beans "list" view was not loaded properly!'
+		);
+		await driver.wait(until.elementLocated(By.className(locators.BeansEditor.detailsView)),
+			5_000, 'Beans "details" view was not loaded properly!'
+		);
+	}
+});
