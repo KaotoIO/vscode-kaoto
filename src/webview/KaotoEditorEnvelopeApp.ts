4f032323f049983e67f7443fa4818a99d41aa476
Dominik Jelínek <djelinek@redhat.com>
2024-01-26 11:25:21 +0100

    Remove keyboard shortcut icon (#459)
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- src/webview/KaotoEditorEnvelopeApp.ts
+++ src/webview/KaotoEditorEnvelopeApp.ts
@@ -1,5 +1,6 @@
 import { KaotoEditorFactory } from '@kaoto-next/ui';
 import { Editor, EditorFactory, KogitoEditorChannelApi } from '@kie-tools-core/editor/dist/api';
+import { NoOpKeyboardShortcutsService } from "@kie-tools-core/keyboard-shortcuts/dist/envelope";
 import * as EditorEnvelope from '@kie-tools-core/editor/dist/envelope';
 declare const acquireVsCodeApi: any;
 
@@ -7,4 +8,5 @@ EditorEnvelope.init({
   container: document.getElementById('envelope-app')!,
   bus: acquireVsCodeApi(),
   editorFactory: new KaotoEditorFactory() as unknown as EditorFactory<Editor, KogitoEditorChannelApi>,
+  keyboardShortcutsService: new NoOpKeyboardShortcutsService()
 });
