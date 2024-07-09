93bda53c16411f291860eb12a28ccd27b80b4899
Dominik Jelinek <djelinek@redhat.com>
2024-07-09 23:15:19 +0200

    test: Add initial UI test for custom catalog URL user setting
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>
    
    fix for settings test failures
    
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>
    
    trying to stabilise mainly on macOS
    
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

---
+++ it-tests/CatalogURLSettings.test.ts
@@ -0,0 +1,121 @@
+import { ActivityBar, after, By, TextSetting, until, VSBrowser, WebDriver, WebView, Workbench } from 'vscode-extension-tester';
+import { checkTopologyLoaded, closeEditor, openAndSwitchToKaotoFrame, resetUserSettings } from './Util';
+import { join } from 'path';
+import { expect } from 'chai';
+
+describe('User Settings', function () {
+    this.timeout(90_000);
+
+    const WORKSPACE_FOLDER = join(__dirname, '../test Fixture with speci@l chars');
+    const CATALOG_URL = 'https://raw.githubusercontent.com/KaotoIO/catalogs/main/catalogs/index.json';
+    const CATALOG_SETTINGS_ID = 'kaoto.catalog.url';
+
+    let driver: WebDriver;
+    let kaotoWebview: WebView;
+
+    const locators = {
+        CatalogDropDown: {
+            dropdown: By.xpath(`//button[@data-testid='runtime-selector-list-dropdown']`)
+        },
+        RuntimeSelector: {
+            selector: (label: string) => By.xpath(`//li[@data-testid='runtime-selector-${label}']`)
+        },
+        RuntimeSelectorItems: {
+            list: By.className('pf-v5-c-menu__list'),
+            listItem: By.className('pf-v5-c-menu__list-item'),
+            label: By.className('pf-v5-c-menu__item-text')
+        }
+    }
+
+    before(async function () {
+        this.timeout(60_000);
+        driver = VSBrowser.instance.driver;
+        await VSBrowser.instance.openResources(WORKSPACE_FOLDER);
+        await VSBrowser.instance.waitForWorkbench();
+
+        // provide the Catalog URL using Settings UI editor
+        const settings = await new Workbench().openSettings();
+        const textSetting = await driver.wait(async () => {
+            return await settings.findSetting('Url', 'Kaoto', 'Catalog') as TextSetting;
+        })
+        await textSetting.setValue(CATALOG_URL);
+        await closeEditor('Settings', true);
+
+        // close sidebar
+        await (await new ActivityBar().getViewControl('Explorer'))?.closeView();
+
+        // open the integration file using Kaoto editor
+        kaotoWebview = (await openAndSwitchToKaotoFrame(
+            WORKSPACE_FOLDER,
+            'my.camel.yaml',
+            driver,
+            true
+        )).kaotoWebview;
+        await checkTopologyLoaded(driver);
+    });
+
+    after(async function () {
+        if (kaotoWebview !== undefined) {
+            try {
+                await kaotoWebview.switchBack();
+            } catch {
+                // probably test not failed in Kaoto UI, just continue
+            }
+        }
+        resetUserSettings(CATALOG_SETTINGS_ID);
+        // the editor in this step needs to be closed using command palette
+        // because in some cases, specially on Windows, there was hover displayed which was blocking the editor close button
+        await new Workbench().executeCommand('View: Close Editor');
+    });
+
+    const runtimeSelectors = ['Main', 'Quarkus', 'SpringBoot'];
+
+    runtimeSelectors.forEach(function (runtime) {
+
+        it(`Check custom Kaoto catalog loaded - ${runtime}`, async function () {
+            this.timeout(60_000);
+
+            // expand Catalog dropdown
+            const dropdown = await driver.findElement(locators.CatalogDropDown.dropdown);
+            await dropdown.click();
+            await driver.wait(
+                until.elementLocated(locators.RuntimeSelectorItems.list
+                ), 5_000, 'Dropdown was not displayed properly!');
+
+            // get all items available for selected runtime
+            let items = await getDropdownItemsText(runtime);
+            expect(items.length).is.greaterThan(1);
+
+            // collapse catalog dropdown
+            await dropdown.click();
+            try {
+                await driver.wait(
+                    until.elementLocated(locators.RuntimeSelectorItems.list
+                    ), 5_000);
+                throw new Error('Dropdown was not closed!')
+            } catch (error) {
+                if (error.name !== 'TimeoutError') {
+                    throw new Error(error.message);
+                }
+            }
+        });
+    });
+
+
+    async function getDropdownItemsText(item: string): Promise<string[]> {
+        // expand selected runtime list
+        const parentItem = await driver.findElement(locators.RuntimeSelector.selector(item));
+        await parentItem.click();
+
+        // get all listed items
+        const items = await parentItem.findElement(locators.RuntimeSelectorItems.list).findElements(locators.RuntimeSelectorItems.listItem);
+
+        // get labels of all displayed items
+        let labels: string[] = [];
+        for (const item of items) {
+            const label = await item.findElement(locators.RuntimeSelectorItems.label).getText();
+            labels.push(label);
+        }
+        return labels;
+    }
+});
--- it-tests/Util.ts
+++ it-tests/Util.ts
@@ -1,6 +1,8 @@
 import { assert } from 'chai';
 import * as path from 'path';
-import { By, CustomEditor, until, VSBrowser, WebDriver, WebView } from 'vscode-extension-tester';
+import * as fs from 'node:fs';
+import * as os from 'os';
+import { By, CustomEditor, EditorView, ModalDialog, TextEditor, until, VSBrowser, WebDriver, WebView } from 'vscode-extension-tester';
 
 export async function openAndSwitchToKaotoFrame(
   workspaceFolder: string,
@@ -46,4 +48,37 @@ export async function checkEmptyCanvasLoaded(driver: WebDriver) {
 
 export async function checkTopologyLoaded(driver: WebDriver) {
   await driver.wait(until.elementLocated(By.xpath("//div[@data-test-id='topology']")));
+}
+
+// Enforce same default storage setup as ExTester - see https://github.com/redhat-developer/vscode-extension-tester/wiki/Test-Setup#useful-env-variables
+export const storageFolder = process.env.TEST_RESOURCES ? process.env.TEST_RESOURCES : `${os.tmpdir()}/test-resources`;
+
+/**
+ * Reset user setting to default value by deleting item in settings.json.
+ *
+ * @param id ID of setting to reset.
+ */
+export function resetUserSettings(id: string): void {
+	const settingsPath = path.resolve(storageFolder, 'settings', 'User', 'settings.json');
+	const reset = fs.readFileSync(settingsPath, 'utf-8').replace(new RegExp(`"${id}.*`), '').replace(/,(?=[^,]*$)/, '');
+	fs.writeFileSync(settingsPath, reset, 'utf-8');
+}
+
+/**
+ * Close editor with handling of 'Save/Don't Save' Modal dialog.
+ *
+ * @param title Title of opened active editor.
+ * @param save true/false
+ */
+export async function closeEditor(title: string, save?: boolean) {
+	const dirty = await new TextEditor().isDirty();
+	await new EditorView().closeEditor(title);
+	if (dirty) {
+		const dialog = new ModalDialog();
+		if (save) {
+			await dialog.pushButton('Save');
+		} else {
+			await dialog.pushButton('Don\'t Save');
+		}
+	}
 }
