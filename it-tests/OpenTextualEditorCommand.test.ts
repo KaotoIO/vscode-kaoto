746ab32ac7f99e1de89febaf878d580fd79f0491
Aurélien Pupier <apupier@redhat.com>
2024-08-08 14:50:37 +0200

    Provide quick action to open textual editor
    fixes #493
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- it-tests/OpenTextualEditorCommand.test.ts
+++ it-tests/OpenTextualEditorCommand.test.ts
@@ -7,7 +7,7 @@ import {
 import { expect } from 'chai';
 import * as path from 'path';
 
-describe('Open Textual editor to the side', function () {
+describe.only('Open Textual editor to the side', function () {
   this.timeout(20_000);
 
   const workspaceFolder = path.join(__dirname, '../test Fixture with speci@l chars');
@@ -22,15 +22,32 @@ describe('Open Textual editor to the side', function () {
   });
 
   it('Use command from palette', async function () {
-    const filePath = path.join(workspaceFolder, 'my.camel.yaml');
-    await VSBrowser.instance.openResources(filePath);
-    const editorView = new EditorView();
-    expect(await editorView.getEditorGroups()).to.have.length(1);
+    const editorView = await openFileInKaotoEditor(workspaceFolder);
 
     await new Workbench().executeCommand('Open Camel file with textual editor on the side');
 
-    expect(await editorView.getEditorGroups()).to.have.length(2);
-    const editor = new TextEditor(await editorView.getEditorGroup(1));
-    expect(await editor.getTextAtLine(1)).contains('- route:');
+    await checkTextualEditorIsOpenedOnTheSide(editorView);
+  });
+
+  it('Use quick action', async function () {
+    const editorView = await openFileInKaotoEditor(workspaceFolder);
+
+    (await editorView.getAction('Open Camel file with textual editor on the side'))?.click();
+
+    await checkTextualEditorIsOpenedOnTheSide(editorView);
   });
 });
+
+async function checkTextualEditorIsOpenedOnTheSide(editorView: EditorView) {
+  expect(await editorView.getEditorGroups()).to.have.length(2);
+  const editor = new TextEditor(await editorView.getEditorGroup(1));
+  expect(await editor.getTextAtLine(1)).contains('- route:');
+}
+
+async function openFileInKaotoEditor(workspaceFolder: string) {
+  const filePath = path.join(workspaceFolder, 'my.camel.yaml');
+  await VSBrowser.instance.openResources(filePath);
+  const editorView = new EditorView();
+  expect(await editorView.getEditorGroups()).to.have.length(1);
+  return editorView;
+}
