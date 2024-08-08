78568bbc53cf54b41893d00809734b416648c1be
Aurélien Pupier <apupier@redhat.com>
2024-08-08 14:50:37 +0200

    Provide quick action to open textual editor
    fixes #493
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- it-tests/OpenTextualEditorCommand.test.ts
+++ it-tests/OpenTextualEditorCommand.test.ts
@@ -6,6 +6,7 @@ import {
 } from 'vscode-extension-tester';
 import { expect } from 'chai';
 import * as path from 'path';
+import * as os from 'os';
 
 describe('Open Textual editor to the side', function () {
   this.timeout(20_000);
@@ -22,15 +23,41 @@ describe('Open Textual editor to the side', function () {
   });
 
   it('Use command from palette', async function () {
-    const filePath = path.join(workspaceFolder, 'my.camel.yaml');
-    await VSBrowser.instance.openResources(filePath);
-    const editorView = new EditorView();
-    expect(await editorView.getEditorGroups()).to.have.length(1);
+    const editorView = await openFileInKaotoEditor(workspaceFolder);
 
     await new Workbench().executeCommand('Open Camel file with textual editor on the side');
 
-    expect(await editorView.getEditorGroups()).to.have.length(2);
-    const editor = new TextEditor(await editorView.getEditorGroup(1));
-    expect(await editor.getTextAtLine(1)).contains('- route:');
+    await checkTextualEditorIsOpenedOnTheSide(editorView);
+  });
+
+  it('Use quick action', async function () {
+    const editorView = await openFileInKaotoEditor(workspaceFolder);
+
+    let actionTitle;
+    if(os.platform() === 'darwin') {
+      actionTitle = 'Open Camel file with textual editor on the side (⌘K V)';
+    } else {
+      actionTitle = 'Open Camel file with textual editor on the side (Ctrl+K V)';
+    }
+    await (await editorView.getAction(actionTitle))?.click();
+
+    await checkTextualEditorIsOpenedOnTheSide(editorView);
   });
 });
+
+async function checkTextualEditorIsOpenedOnTheSide(editorView: EditorView) {
+  const driver = VSBrowser.instance.driver;
+  await driver.wait(async() => {
+    return (await editorView.getEditorGroups()).length === 2;
+  }, 5000, 'The second editor group has not opened');
+  const editor = new TextEditor(await editorView.getEditorGroup(1));
+  expect(await editor.getTextAtLine(1)).contains('- route:');
+}
+
+async function openFileInKaotoEditor(workspaceFolder: string) {
+  const filePath = path.join(workspaceFolder, 'my.camel.yaml');
+  await VSBrowser.instance.openResources(filePath);
+  const editorView = new EditorView();
+  expect(await editorView.getEditorGroups()).to.have.length(1);
+  return editorView;
+}
