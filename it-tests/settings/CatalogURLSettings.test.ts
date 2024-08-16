9485a2f1a5437d1ae002caae883fc15606d0288a
Dominik Jelínek <djelinek@redhat.com>
2024-08-16 18:43:01 +0200

    test: improve user settings tests, select catalog and validate available components (#636)

--- it-tests/settings/CatalogURLSettings.test.ts
+++ it-tests/settings/CatalogURLSettings.test.ts
@@ -1,10 +1,10 @@
-import { ActivityBar, after, By, TextSetting, until, VSBrowser, WebDriver, WebView, Workbench } from 'vscode-extension-tester';
+import { ActivityBar, after, By, Key, TextSetting, until, VSBrowser, WebDriver, WebElement, WebView, Workbench } from 'vscode-extension-tester';
 import { checkTopologyLoaded, closeEditor, openAndSwitchToKaotoFrame, resetUserSettings } from '../Util';
 import { join } from 'path';
 import { expect } from 'chai';
 
 describe('User Settings', function () {
-    this.timeout(90_000);
+    this.timeout(240_000);
 
     const WORKSPACE_FOLDER = join(__dirname, '../../test Fixture with speci@l chars');
     const CATALOG_URL = 'https://raw.githubusercontent.com/KaotoIO/catalogs/main/catalogs/index.json';
@@ -24,6 +24,19 @@ describe('User Settings', function () {
             list: By.className('pf-v5-c-menu__list'),
             listItem: By.className('pf-v5-c-menu__list-item'),
             label: By.className('pf-v5-c-menu__item-text')
+        },
+        KaotoView: {
+            catalogButton: By.xpath(`//button[@id='topology-control-bar-catalog-button']`),
+            catalog: {
+                window: By.xpath(`//div[@data-ouia-component-id='CatalogModal']`),
+                list: By.className('pf-v5-c-data-list'),
+                gallery: By.className('pf-v5-l-gallery pf-m-gutter'),
+                listItem: By.className('pf-v5-c-data-list__item-row'),
+                listItemProvider: By.className('catalog-data-list-item__provider'),
+                listButton: By.xpath(`//button[@id='toggle-layout-button-List']`),
+                galleryButton: By.xpath(`//button[@id='toggle-layout-button-Gallery']`),
+                closeButton: By.xpath(`//button[@data-ouia-component-id='CatalogModal-ModalBoxCloseButton']`)
+            }
         }
     }
 
@@ -77,11 +90,7 @@ describe('User Settings', function () {
             this.timeout(60_000);
 
             // expand Catalog dropdown
-            const dropdown = await driver.findElement(locators.CatalogDropDown.dropdown);
-            await dropdown.click();
-            await driver.wait(
-                until.elementLocated(locators.RuntimeSelectorItems.list
-                ), 5_000, 'Dropdown was not displayed properly!');
+            const dropdown = await expandCatalogDropdown();
 
             // get all items available for selected runtime
             let items = await getDropdownItemsText(runtime);
@@ -102,6 +111,67 @@ describe('User Settings', function () {
         });
     });
 
+    it(`Select 'redhat' catalog and check new components are available`, async function () {
+        this.timeout(90_000);
+
+        // expand dropdown
+        const dropdown = await expandCatalogDropdown();
+
+        // select Main > Camel Main *redhat*
+        const parentItem = await dropdown.findElement(locators.RuntimeSelector.selector('Main'));
+        await parentItem.click();
+
+        // get items and click on last which is currently always the 'redhat' one
+        const items = await parentItem.findElement(locators.RuntimeSelectorItems.list).findElements(locators.RuntimeSelectorItems.listItem);
+        await items.at(-1)?.click();
+
+        // it needs some time to start loading a new catalog
+        await driver.sleep(1_000);
+
+        // wait for reload of kaoto view
+        await checkTopologyLoaded(driver, 15_000);
+
+        // click Open Catalog
+        const catalog = await driver.findElement(locators.KaotoView.catalogButton);
+        await catalog.click();
+
+        // wait catalog modal dialog is open
+        await driver.wait(until.elementLocated(locators.KaotoView.catalog.window), 15_000);
+        const catalogWindow = await driver.findElement(locators.KaotoView.catalog.window);
+
+        // switch to list view
+        await catalogWindow.findElement(locators.KaotoView.catalog.listButton).click();
+        await driver.wait(until.elementLocated(locators.KaotoView.catalog.list), 5_000);
+
+        // check first component contains 'Red Hat' flag
+        const listItem = await catalogWindow.findElement(locators.KaotoView.catalog.list).findElement(locators.KaotoView.catalog.listItem);
+        const provider = await listItem.findElement(locators.KaotoView.catalog.listItemProvider).getText();
+        expect(provider).to.be.equal('Red Hat');
+
+        // switch catalog window back to gallery view
+        try {
+            await catalogWindow.findElement(locators.KaotoView.catalog.galleryButton).click();
+            await driver.wait(until.elementLocated(locators.KaotoView.catalog.gallery), 5_000);
+        } catch (error) {
+            // it can happen that because of hover text for list view button
+            // the gallery view button is overlapped and it is not clickable at the moment
+            await driver.actions().sendKeys(Key.ENTER).perform(); // WORKAROUND
+            await catalogWindow.findElement(locators.KaotoView.catalog.galleryButton).click();
+            await driver.wait(until.elementLocated(locators.KaotoView.catalog.gallery), 5_000);
+        }
+
+        // close catalog view
+        await catalogWindow.findElement(locators.KaotoView.catalog.closeButton).click();
+    });
+
+    async function expandCatalogDropdown(timeout: number = 5_000): Promise<WebElement> {
+        const dropdown = await driver.findElement(locators.CatalogDropDown.dropdown);
+        await dropdown.click();
+        await driver.wait(
+            until.elementLocated(locators.RuntimeSelectorItems.list
+            ), timeout, 'Dropdown was not displayed properly!');
+        return dropdown;
+    }
 
     async function getDropdownItemsText(item: string): Promise<string[]> {
         // expand selected runtime list
