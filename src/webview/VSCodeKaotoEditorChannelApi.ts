6f984fa8fc2e81667749961438b6fba9e0b6115a
Aurélien Pupier <apupier@redhat.com>
2024-09-13 14:59:46 +0200

    Provide empty implementation for new Channel API relative to metadata an fileContent
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/webview/VSCodeKaotoEditorChannelApi.ts
+++ src/webview/VSCodeKaotoEditorChannelApi.ts
@@ -1,9 +1,35 @@
 import { KaotoEditorChannelApi } from '@kaoto/kaoto';
 import { ISettingsModel, NodeLabelType, SettingsModel } from '@kaoto/kaoto/models';
+import { BackendProxy } from '@kie-tools-core/backend/dist/api';
+import { NotificationsChannelApi } from "@kie-tools-core/notifications/dist/api";
+import { ResourceContentService, WorkspaceChannelApi } from "@kie-tools-core/workspace/dist/api";
+import { I18n } from '@kie-tools-core/i18n/dist/core';
 import { DefaultVsCodeKieEditorChannelApiImpl } from '@kie-tools-core/vscode-extension/dist/DefaultVsCodeKieEditorChannelApiImpl';
+import { VsCodeI18n } from '@kie-tools-core/vscode-extension/dist/i18n';
+import { VsCodeKieEditorController } from '@kie-tools-core/vscode-extension/dist/VsCodeKieEditorController';
+import { JavaCodeCompletionApi } from '@kie-tools-core/vscode-java-code-completion/dist/api';
 import * as vscode from 'vscode';
+import { VsCodeKieEditorCustomDocument } from '@kie-tools-core/vscode-extension/dist/VsCodeKieEditorCustomDocument';
+import * as path from 'path';
 
 export class VSCodeKaotoEditorChannelApi extends DefaultVsCodeKieEditorChannelApiImpl implements KaotoEditorChannelApi {
+
+  private readonly currentEditedDocument: vscode.TextDocument | VsCodeKieEditorCustomDocument;
+
+  constructor(
+    editor: VsCodeKieEditorController,
+    resourceContentService: ResourceContentService,
+    workspaceApi: WorkspaceChannelApi,
+    backendProxy: BackendProxy,
+    notificationsApi: NotificationsChannelApi,
+    javaCodeCompletionApi: JavaCodeCompletionApi,
+    viewType: string,
+    i18n: I18n<VsCodeI18n>
+  ) {
+    super(editor, resourceContentService, workspaceApi, backendProxy, notificationsApi, javaCodeCompletionApi, viewType, i18n);
+    this.currentEditedDocument = editor.document.document;
+  }
+
   async getCatalogURL(): Promise<string | undefined> {
     return await vscode.workspace.getConfiguration('kaoto').get('catalog.url');
   }
@@ -18,5 +44,23 @@ export class VSCodeKaotoEditorChannelApi extends DefaultVsCodeKieEditorChannelAp
     };
 
     return new SettingsModel(settingsModel);
+  }
+
+  async getMetadata<T>(key: string): Promise<T | undefined> {
+    vscode.window.showErrorMessage(`getMetadata Not implemented. It was called with ${key}`);
+    return undefined;
+  }
+
+  async setMetadata<T>(key: string, value: T | undefined): Promise<void> {
+    vscode.window.showErrorMessage(`setMetadata Not implemented. It was called with ${key} and ${value}`);
+  }
+
+  async getResourceContent(relativePath: string): Promise<string | undefined> {
+    vscode.window.showErrorMessage(`getResourceContent Not implemented. It was called with ${relativePath}`);
+    return undefined;
+  }
+
+  async saveResourceContent(relativePath: string, content: string): Promise<void> {
+    vscode.window.showErrorMessage(`saveResourceContent Not implemented. It was called with ${relativePath} and ${content}`);
   }
 }
