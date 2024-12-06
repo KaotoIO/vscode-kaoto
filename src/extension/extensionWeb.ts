2ad457432e5fd0af90f780910deb248beb11b8d6
Dominik Jelinek <djelinek@redhat.com>
2024-12-06 01:36:52 +0100

    chore: unify copyright header in all .ts files

--- src/extension/extensionWeb.ts
+++ src/extension/extensionWeb.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2024 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { backendI18nDefaults, backendI18nDictionaries } from "@kie-tools-core/backend/dist/i18n";
 import { VsCodeBackendProxy } from "@kie-tools-core/backend/dist/vscode";
 import { EditorEnvelopeLocator, EnvelopeContentType, EnvelopeMapping } from "@kie-tools-core/editor/dist/api";
