723ad4c31adaa269c58d9fc57462acdcdb1ce6a5
Aurélien Pupier <apupier@redhat.com>
2025-01-02 11:32:13 +0100

    add trace

--- src/webview/VSCodeKaotoEditorChannelApi.ts
+++ src/webview/VSCodeKaotoEditorChannelApi.ts
@@ -158,6 +158,8 @@ export class VSCodeKaotoEditorChannelApi extends DefaultVsCodeKieEditorChannelAp
         kaotoMetadataFile = await this.findKaotoMetadataToCreate();
       }
       return await vscode.window.showQuickPick(files.map((f) => {
+        logInKaotoOutputChannel('askUserForFileSelection picked path' + f.path, undefined);
+        logInKaotoOutputChannel('askUserForFileSelection dirname kaoto metadata path' + path.dirname(kaotoMetadataFile.fsPath), undefined);
         logInKaotoOutputChannel('askUserForFileSelection returned value' + path.relative(path.dirname(kaotoMetadataFile.fsPath), f.path), undefined);
         return path.relative(path.dirname(kaotoMetadataFile.fsPath), f.path);
       }), options as vscode.QuickPickOptions);
