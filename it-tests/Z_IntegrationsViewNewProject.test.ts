bcdfafefc2b8874dc5d27d87f75c89ba9fa41ddf
Dominik Jelinek <djelinek@redhat.com>
2025-03-07 15:10:41 +0100

    test: add basic UI test for New Camel project cretion using Export button

---
+++ it-tests/Z_IntegrationsViewNewProject.test.ts
@@ -0,0 +1,150 @@
+/**
+ * Copyright 2025 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { expect } from 'chai';
+import { join } from 'path';
+import fs from 'fs';
+import {
+	ActivityBar,
+	after,
+	before,
+	EditorView,
+	InputBox,
+	ModalDialog,
+	SideBarView,
+	TreeItem,
+	until,
+	ViewControl,
+	ViewItemAction,
+	ViewSection,
+	VSBrowser,
+	WebDriver,
+} from 'vscode-extension-tester';
+
+/**
+ * This test needs to be always executed as last in suite
+ */
+describe('Integrations View', function () {
+	this.timeout(240_000);
+
+	const WORKSPACE_FOLDER = join(__dirname, '../test Fixture with speci@l chars', 'kaoto-view');
+
+	let driver: WebDriver;
+	let kaotoViewContainer: ViewControl | undefined;
+	let kaotoView: SideBarView | undefined;
+	let integrationsSection: ViewSection | undefined;
+
+	before(async function () {
+		driver = VSBrowser.instance.driver;
+		await VSBrowser.instance.openResources(WORKSPACE_FOLDER);
+		await VSBrowser.instance.waitForWorkbench();
+
+		kaotoViewContainer = await new ActivityBar().getViewControl('Kaoto');
+		kaotoView = await kaotoViewContainer?.openView();
+		integrationsSection = await kaotoView?.getContent().getSection('Integrations');
+	});
+
+	after(async function () {
+		await kaotoViewContainer?.closeView();
+		await new EditorView().closeAllEditors();
+	});
+
+	it(`'Export' button is available`, async function () {
+		const item = await getTreeItem('kam1.kamelet.yaml');
+		const exportButton = await item?.getActionButton('Export');
+		expect(exportButton).to.not.be.undefined;
+	});
+
+	/**
+	 * Skip on macOS - see https://issues.redhat.com/browse/FUSETOOLS2-2553
+	 */
+	(process.platform === 'darwin' ? describe.skip : describe)(`Click 'Export' button`, function () {
+		const PROJECT_OUTPUT_DIR: string = join(WORKSPACE_FOLDER, 'quarkus-export-example');
+
+		let input: InputBox;
+		let exportButton: ViewItemAction | undefined;
+
+		before(async function () {
+			fs.mkdirSync(PROJECT_OUTPUT_DIR, { recursive: true });
+			const item = await getTreeItem('sample1.camel.yaml');
+			exportButton = await item?.getActionButton('Export');
+			await exportButton?.click();
+		});
+
+		after(async function () {
+			await new EditorView().closeAllEditors();
+			fs.rmSync(PROJECT_OUTPUT_DIR, { force: true, recursive: true });
+		});
+
+		it(`Create a new Quarkus project`, async function () {
+			// runtime selection
+			input = await InputBox.create(30_000);
+			await input.setText('quarkus');
+			await input.confirm();
+
+			// GAV selection
+			input = await InputBox.create(30_000);
+			await input.confirm();
+
+			// specify new project output dir
+			input = await InputBox.create(30_000);
+			await input.setText(PROJECT_OUTPUT_DIR);
+			await input.confirm();
+			await input.confirm(); // for some reason when using setText for a path pick input it needs to be confirmed twice (see https://github.com/redhat-developer/vscode-extension-tester/issues/1778)
+
+			const dialog = new ModalDialog();
+			await driver.wait(
+				async function () {
+					return await dialog.isDisplayed();
+				},
+				10_000,
+				'Modal Dialog was not displayed properly!',
+			);
+			await dialog.pushButton('Continue');
+			await dialog.getDriver().wait(until.stalenessOf(dialog), 2_500, 'Dialog did not disappeared');
+
+			await waitUntilNewCamelProjectHasCrucialFiles();
+		});
+
+		async function waitUntilNewCamelProjectHasCrucialFiles(): Promise<void> {
+			await driver.wait(
+				async function () {
+					const items = (await integrationsSection?.getVisibleItems()) as TreeItem[];
+					const item = items.find(async (item) => {
+						return (await item.getDescription()) === 'quarkus-export-example/src/main/resources/camel';
+					});
+					return item && fs.existsSync(join(PROJECT_OUTPUT_DIR, 'pom.xml'));
+				},
+				180_000,
+				`New Camel Project was not created properly!`,
+				2_000,
+			);
+		}
+	});
+
+	async function getTreeItem(filename: string): Promise<TreeItem | undefined> {
+		return await driver.wait(
+			async function () {
+				try {
+					return (await integrationsSection?.findItem(filename)) as TreeItem;
+				} catch (error) {
+					return undefined;
+				}
+			},
+			10_000,
+			`${filename} was not found within Integrations view!`,
+		);
+	}
+});
