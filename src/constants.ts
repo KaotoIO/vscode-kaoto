409ffcf5f8a3f0cadf8fb87a511ee1eaa74b049b
Dominik Jelinek <djelinek@redhat.com>
2026-04-09 11:53:24 +0200

    fix: use static camel jbang version definition

--- src/constants.ts
+++ src/constants.ts
@@ -10,3 +10,9 @@ import packageJson from '../package.json';
  * This value is defined in package.json under "kaoto.defaultCamelVersion"
  */
 export const DEFAULT_CAMEL_VERSION = packageJson.kaoto?.defaultCamelVersion || '4.18.1';
+
+/**
+ * Default Camel JBang version used for JBang executor
+ * This value is defined in package.json under "kaoto.defaultCamelJbangVersion"
+ */
+export const DEFAULT_CAMEL_JBANG_VERSION = packageJson.kaoto?.defaultCamelJbangVersion || '4.18.1';
--- src/executors/JBangExecutor.ts
+++ src/executors/JBangExecutor.ts
@@ -6,6 +6,7 @@ import { execSync } from 'child_process';
 import { BaseExecutor } from './BaseExecutor';
 import { CamelCommandBuilder } from './builders/CamelCommandBuilder';
 import { JBangExecutorConfig } from './types/ExecutorConfig';
+import { DEFAULT_CAMEL_JBANG_VERSION } from '../constants';
 
 /**
  * JBang executor implementation
@@ -14,10 +15,10 @@ export class JBangExecutor extends BaseExecutor {
 	constructor(config: JBangExecutorConfig) {
 		const jbangPath = config.jbangPath || 'jbang';
 
-		// Configure builder for JBang
+		// Configure builder for JBang - always use default JBang version from package.json
 		const commandBuilder = new CamelCommandBuilder({
 			executable: jbangPath,
-			prefixArgs: [`-Dcamel.jbang.version=${config.version}`, 'camel@apache/camel'],
+			prefixArgs: ['--verbose', `-Dcamel.jbang.version=${DEFAULT_CAMEL_JBANG_VERSION}`, 'camel@apache/camel'],
 		});
 
 		super(config, commandBuilder);
